4017701729f14fd47e80eba6297d758e687ca27a
Esther Ndep <ndepeok@aston.ac.uk>
2018-10-05 20:09:03 +0100

    Add Icons to services page

--- resources/views/pages/services.blade.php
+++ resources/views/pages/services.blade.php
@@ -23,7 +23,7 @@
                 
             </section>
             <section id="service-content">
-                <div class="row ml-auto mr-auto" style="margin-top: 25rem; width:90%">
+                <div class="service-block row ml-auto mr-auto" id ="residential" style="margin-top: 25rem; width:90%">
                         <div class="sngl-service">
                                 <div class="col-12" >
                                     <h3 style=" margin-bottom:2rem;">UNMATCHED
@@ -31,59 +31,97 @@
                                             UNMATCHED
                                             EXCELLENCE.
                                     </h3>
-                                     <span style="font-size:0.5rem;" ><a class="pr-2" href="">RESIDENTIAL </a>/<a class="pr-2 pl-2" href="">COMMERCIAL</a>/<a class="pl-2" href="">HOSPITALITY</a></span>
+                                     <span style="font-size:0.5rem;" ><a class="pr-2" href="">RESIDENTIAL </a>/
+                                     <a class="pr-2 pl-2" href="">COMMERCIAL</a>/
+                                     <a class="pl-2" href="">HOSPITALITY</a></span>
 
                                 </div>
                         </div>
                         <div class="sngl-service">
                                 <div class="sngl-inner">
-                                                <div class="sngl-icon">
-                                                        
-                                                </div>
+                                        <div class="sngl-icon">
+                                                <i class="i-lamp"></i>     
+                                        </div>
                                 </div>
                         </div>
                         <div class="sngl-service">
                                 <div class="sngl-inner">
-                                        <div class="sngl-icon"></div>
+                                        <div class="sngl-icon">
+                                                <i class="i-camera"></i>
+                                        </div>
                                 </div>
                         </div>
                         <div class="sngl-service">
                                 <div class="sngl-inner">
-                                        <div class="sngl-icon"></div>
+                                        <div class="sngl-icon">
+                                                <i class="i-safe"></i>
+                                        </div>
                                 </div>
                         </div>
                         <div class="sngl-service">
                                 <div class="sngl-inner">
-                                        <div class="sngl-icon"></div>
+                                        <div class="sngl-icon">
+                                                <i class="i-radio"></i>
+                                        </div>
                                 </div>
                         </div>
                         <div class="sngl-service">
                                 <div class="sngl-inner">
-                                        <div class="sngl-icon"></div>
+                                        <div class="sngl-icon">
+                                                <i class="i-film1"></i>
+                                        </div>
+                                </div>
+                        </div>
+                </div>
+                <div class="service-block row ml-auto mr-auto" id ="commercial" style="margin-top: 25rem; width:90%">
+                        <div class="sngl-service">
+                                <div class="col-12" >
+                                    <h3 style=" margin-bottom:2rem;">UNMATCHED
+                                            SERVICES.
+                                            UNMATCHED
+                                            EXCELLENCE.
+                                    </h3>
+                                     <span style="font-size:0.5rem;" ><a class="pr-2" href="">RESIDENTIAL </a>/
+                                     <a class="pr-2 pl-2" href="">COMMERCIAL</a>/
+                                     <a class="pl-2" href="">HOSPITALITY</a></span>
+
                                 </div>
                         </div>
                         <div class="sngl-service">
                                 <div class="sngl-inner">
-                                        <div class="sngl-icon"></div>
-                                </div>
-                        </div>
-                
-                        <div class="sngl-service">
-                                <div class="sngl-inner">
-                                        <div class="sngl-icon"></div>
+                                        <div class="sngl-icon">
+                                                <i class="i-profile"></i>     
+                                        </div>
                                 </div>
                         </div>
                         <div class="sngl-service">
                                 <div class="sngl-inner">
-                                         <div class="sngl-icon"></div>
+                                        <div class="sngl-icon">
+                                                <i class="i-lamp"></i>
+                                        </div>
                                 </div>
                         </div>
                         <div class="sngl-service">
                                 <div class="sngl-inner">
-                                        <div class="sngl-icon"></div>
+                                        <div class="sngl-icon">
+                                                <i class="i-presentation"></i>
+                                        </div>
                                 </div>
                         </div>
-                
+                        <div class="sngl-service">
+                                <div class="sngl-inner">
+                                        <div class="sngl-icon">
+                                                <i class="i-users"></i>
+                                        </div>
+                                </div>
+                        </div>
+                        <div class="sngl-service">
+                                <div class="sngl-inner">
+                                        <div class="sngl-icon">
+                                                <i class="i-contrast"></i>
+                                        </div>
+                                </div>
+                        </div>
                 </div>
             </section>
     </div>
